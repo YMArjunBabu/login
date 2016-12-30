812c5ed0b09971c22a4d694db26343f355350db7
ArjunBabuYM <arjun@processbricks.com>
2016-12-30 16:25:16 +0530

    port

--- host.js
+++ host.js
@@ -2,7 +2,7 @@ var bouncy = require('bouncy');
 var server = bouncy(function (req, res, bounce) {
   console.log(req.headers.host);
     if (req.headers.host === 'http://192.168.1.88:8027') {
-        bounce(8029);
+        bounce(8026);
     }else {
       res.statusCode = 404;
       res.end('no such host');
