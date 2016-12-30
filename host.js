b70f0e9dded3bc740e3c0c267f2c24f748fbfe14
ArjunBabuYM <arjun@processbricks.com>
2016-12-30 16:35:04 +0530

    url

--- host.js
+++ host.js
@@ -1,7 +1,7 @@
 var bouncy = require('bouncy');
 var server = bouncy(function (req, res, bounce) {
   console.log(req.headers.host);
-    if (req.headers.host === 'http://192.168.1.88:8027/login') {
+    if (req.headers.host === 'http://192.168.1.88:8027') {
         bounce(8026);
     }else {
       res.statusCode = 404;
